92476761314719ac3103bd45bf122d74af4bec9c
David Simbolon <davidhedrin123@gmail.com>
2023-05-09 17:51:33 +0700

    first commit

---
+++ halpers/collections.js
@@ -0,0 +1,30 @@
+var collectionsName = {
+    kode_jemaat : "kode_jemaat",
+    users : "users",
+    panding_user : "panding_user",
+    kolom_jemaat : "kolom_jemaat",
+    jadwal_jemaat : "jadwal_jemaat",
+}
+
+var collInCollsName = {
+    tb_kolom_jemaat : {
+        collSabtuSabat : "sabtu_sabat",
+        collKhotbahSabat : "khotbah_sabat",
+        collRabuMalam : "rabu_malam",
+        collJumatVesper : "jumat_vesper",
+    },
+    tb_kode_jemaat : {
+        collAnggotaJemaat : "anggota_jemaat",
+    }
+}
+
+var collumnsName = {
+    tb_kolom_jemaat : {
+        col_jemaat_id : "jemaat_id",
+        col_urutan : "posisi"
+    },
+}
+
+var customParam = {
+    kode_schadule : "kode_schadule",
+}
---
+++ halpers/pubic_function.js
@@ -0,0 +1,186 @@
+var generalSetting = {
+  firebaseConfig: function firebaseConfig() {
+    return {
+      apiKey: "AIzaSyAzcfKBs_ExATPPU6PmMnOpmdksZMQ5bSE",
+      authDomain: "schadule-app-jquery.firebaseapp.com",
+      projectId: "schadule-app-jquery",
+      storageBucket: "schadule-app-jquery.appspot.com",
+      messagingSenderId: "498795039698",
+      appId: "1:498795039698:web:0f4b58d0a81643ad99560b"
+    }
+  }
+}
+
+function redirect(setUrl) {
+  // For site web
+  var siteUrl = window.location.origin;
+
+  // For local site
+  var localUrl = window.location.href;
+  var index = localUrl.lastIndexOf('/');
+  var newStr = localUrl.substring(0, index);
+  var siteLocal = newStr + "/";
+
+  window.location.href = siteLocal + setUrl;
+}
+
+
+function getLinkPageUrlParam(setUrl) {
+  // For site web
+  var siteUrl = window.location.origin;
+
+  // For local site
+  var localUrl = window.location.href;
+  var index = localUrl.lastIndexOf('/');
+  var newStr = localUrl.substring(0, index);
+  var siteLocal = newStr + "/";
+
+  return siteLocal + setUrl;
+}
+
+function getParameterUrlByName(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+
+var toastAction = {
+  showToastAlert: function showToastAlert(toastId, title, subtitle = "", message, type) {
+    var titleToast = toastId.find("#title_toast");
+    var subTitleToast = toastId.find("#sub_title_toast");
+    var messageToast = toastId.find("#message_toast");
+
+    titleToast.html(title);
+    subTitleToast.html(subtitle);
+    messageToast.html(message);
+    toastId.removeClass("bg-success");
+    toastId.removeClass("bg-info");
+    toastId.removeClass("bg-warning");
+    toastId.removeClass("bg-danger");
+    toastId.removeClass("show");
+    toastId.removeClass("hide");
+
+    var setType;
+    if (type == "success") {
+      setType = "bg-success";
+    } else if (type == "info") {
+      setType = "bg-info";
+    } else if (type == "warning") {
+      setType = "bg-warning";
+    } else if (type == "danger") {
+      setType = "bg-danger";
+    };
+    toastId.addClass(setType);
+    toastId.addClass("show");
+  },
+  hideToastAlert: function hideToastAlert(toastId) {
+    setTimeout(() => {
+      toastId.removeClass("show");
+    }, 4000);
+  }
+}
+
+function generateGuid() {
+  var resultGuid = "";
+  var guid = 'xxxxxxxxxxxxxx4xxxxyx';
+  guid.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    var result = v.toString(16);
+    resultGuid += result;
+  });
+
+  return resultGuid;
+}
+
+function logOut(e, auth, signOut, onAuthStateChanged) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      redirect("auth/login.html");
+    } else {
+      //e.stopImmediatePropagation();
+      signOut(auth).then(() => {
+        redirect("auth/login.html");
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+  });
+}
+
+function deleteDocumentById(id, collection) {
+  const db = firebase.firestore();
+  return db.collection(collection).doc(id).delete().then(function () {
+    return true;
+  });
+}
+function deleteDocCollInCollById(id1, collection1, id2, collection2) {
+  const db = firebase.firestore();
+  return db.collection(collection1).doc(id1).collection(collection2).doc(id2).delete().then(function () {
+    return true;
+  });
+}
+
+async function getUserLogin(userId) {
+  const db = firebase.firestore();
+  var userById = db.collection(collectionsName.users).doc(userId);
+  var getUserById = await userById.get();
+  var getDataUser = getUserById.data();
+
+  return getDataUser;
+}
+
+function deleteFirestoreCollection(collectionRef) {
+  var db = firebase.firestore();
+  var batchSize = 50;
+  var query = collectionRef.limit(batchSize);
+
+  return query.get().then(function (snapshot) {
+    // When there are no documents left, we are done
+    if (snapshot.size === 0) {
+      return null;
+    }
+
+    // Delete documents in batch
+    var batch = db.batch();
+    snapshot.docs.forEach(function (doc) {
+      batch.delete(doc.ref);
+    });
+
+    // Recurse on the next batch process
+    return batch.commit().then(function () {
+      return deleteFirestoreCollection(collectionRef);
+    });
+  });
+}
+
+function getCurrentTriwulan() {
+  const dt = new Date();
+  var month = dt.getMonth() + 1;
+  var year = dt.getFullYear();
+  var monthsPerGroup = 3;
+  var totalGroups = 4;
+
+  var daysInCurrentGroup = 0;
+  for (var i = 0; i < totalGroups; i++) {
+    var startMonth = i * monthsPerGroup;
+    var endMonth = startMonth + monthsPerGroup - 1;
+
+    if (month >= startMonth && month <= endMonth) {
+      var daysInGroup = 0;
+      for (var j = startMonth; j <= endMonth; j++) {
+        var firstDayOfMonth = new Date(year, j, 1);
+        var firstDayOfNextMonth = new Date(year, firstDayOfMonth.getMonth() + 1, 1);
+        var lastDayOfMonth = new Date(firstDayOfNextMonth.getTime() - 1);
+        var daysInMonth = lastDayOfMonth.getDate();
+        daysInGroup += daysInMonth;
+      }
+
+      daysInCurrentGroup = i + 1;
+      break;
+    }
+  }
+
+  return daysInCurrentGroup;
+}
